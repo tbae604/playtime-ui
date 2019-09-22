32555a50acaeeec8812cf535bdf15f0beb79d414
tk baylis <tammy.baylis@gmail.com>
2019-09-21 22:26:03 -0700

    Split API and UI to separate repos

---
+++ src/components/guestbook.js
@@ -0,0 +1,20 @@
+
+
+
+export class GuestBook extends Component {
+
+    constructor(props) {
+        super(props);
+
+        // !!! bind handlers
+
+        this.state = {
+            user: "",
+            message: "",
+        };
+    }
+
+    handleUser(event) {
+        this.setState({});
+    }
+}
